8971740d85776e77c1448cdcd89c1f28d0423889
ramalakshmidevatha16-wq <ramalakshmidevatha16@gmail.com>
2026-03-05 16:03:00 +0530

    Create HashSet Operations.cpp

---
+++ Phase-1/HashSet Operations.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,m;
+  cin>>n;
+  set <int> a,b;
+  for(int i=0;i<n;i++)
+  {
+    int x;
+    cin>>x;
+    a.insert(x);
+  }
+  cin>>m;
+  for(int i=0;i<m;i++)
+  {
+    
+  }
+    return 0;
+}
